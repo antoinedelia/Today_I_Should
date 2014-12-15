517e5623eb310a3f32a422d0b20b4f9534188da0
antoinedelia <antoine.delia@gmail.com>
2014-12-15 11:20:26 +0100

    First index test
    Title + Description of movie

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Today I Should</title>
+		<meta charset="utf8">
+	</head>
+	<body>
+		<h1>Today I Should...</h1>
+		<h2>Watch/Read...</h2>
+		<h3>Name of movie...</h3>
+		<img src="photo.jpg" alt="">
+		<p>
+			<strong>Description :</strong>
+			Blablabla
+		</p>
+	</body>
+</html>
