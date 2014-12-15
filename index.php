435818a567c507aebb63324dd91d2578e08873ac
antoinedelia <antoine.delia@gmail.com>
2014-12-15 11:42:09 +0100

    Add pure css and theme for website

--- index.php
+++ index.php
@@ -1,18 +1,30 @@
 <!DOCTYPE html>
 <html>
 	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
 		<title>Today I Should</title>
-		<meta charset="utf8">
+
 		<link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.5.0/pure-min.css">
+    	<!--[if lte IE 8]>
+        <link rel="stylesheet" href="css/layouts/side-menu-old-ie.css">
+    <![endif]-->
+    <!--[if gt IE 8]><!-->
+        <link rel="stylesheet" href="css/layouts/side-menu.css">
+    <!--<![endif]-->
 	</head>
 	<body>
-		<h1>Today I Should...</h1>
-		<h2>Watch/Read...</h2>
-		<h3>Name of movie...</h3>
-		<img src="photo.jpg" alt="">
-		<p>
-			<strong>Description :</strong>
-			Blablabla
-		</p>
+	    <div id="main">
+	        <div class="header">
+	            <h1>Today I Should</h1>
+	            <h2>The website that tells you what to do !</h2>
+	        </div>
+
+	        <div class="content">
+	            <h2 class="content-subhead">Today you should...</h2>
+	            <p>
+	        </div>
+	    </div>
 	</body>
 </html>
